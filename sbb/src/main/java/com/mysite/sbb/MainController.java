8dc405452b19aa3d9a384e99f601ad85d59c0709
깃kkh333 <k2ho33333@gmail.com>
2023-11-21 18:55:50 +0900

    2-12 완료

--- sbb/src/main/java/com/mysite/sbb/MainController.java
+++ sbb/src/main/java/com/mysite/sbb/MainController.java
@@ -11,4 +11,12 @@ public class MainController {
     public String index() {
         return "안녕하세요!";
     }
+
+    @GetMapping("/")
+    public String root() {
+        //forward사용하면 http://localhost:8080/ 로 접속했을 때 주소가 그대로
+        //return "forward:/question/list";
+        //redirect사용하면 http://localhost:8080/ 로 접속했을 때 주소가 http://localhost:8080/question/list 로 이동
+        return "redirect:/question/list";
+    }
 }
