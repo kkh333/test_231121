82a7af5a1a18490cc0be39cd51409bee9b06466f
kkh <k2ho33333@gmail.com>
2023-11-22 11:27:35 +0900

    2-15 ã게시글 상세 오류

--- sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -4,6 +4,7 @@ import com.mysite.sbb.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,5 +23,13 @@ public class QuestionService {
             return question.get();
         }
         else throw new DataNotFoundException("question not found");
+    }
+
+    public void create(String subject, String content) {
+        Question question = new Question();
+        question.setSubject(subject);
+        question.setContent(content);
+        question.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(question);
     }
 }
